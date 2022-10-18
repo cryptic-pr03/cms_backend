2006efa325365a682eecfb24a85bb7f311e28444
Priyanshu Raman <92126782+cryptic-pr03@users.noreply.github.com>
2022-10-18 12:51:11 +0530

    added bank details dao

---
+++ src/main/java/com/example/cms/dao/BankDetailsDAO.java
@@ -0,0 +1,19 @@
+package com.example.cms.dao;
+
+import com.example.cms.Models.BankDetails;
+
+import java.util.List;
+
+public interface BankDetailsDAO {
+
+    BankDetails addBankDetails(BankDetails newBankDetails) throws CustomException;
+
+    Boolean deleteBankDetails(BankDetails bankDetails) throws CustomException;
+
+    BankDetails updateBankDetails(BankDetails updatedBankDetails) throws CustomException;
+
+    List<BankDetails> getBankDetailsByAttribute(String attributeName, String attributeValue);
+
+    List<BankDetails> getBankDetailsByUserId(int userId) throws CustomException;
+
+}
