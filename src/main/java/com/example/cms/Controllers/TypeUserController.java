fa2485ea8a720cb0c55d7c922148ecb4ce9b4687
Priyanshu Raman <92126782+cryptic-pr03@users.noreply.github.com>
2022-11-14 09:21:59 +0000

    Update TypeUserController.java

--- src/main/java/com/example/cms/Controllers/TypeUserController.java
+++ src/main/java/com/example/cms/Controllers/TypeUserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000","http://cms-frontend-tau.vercel.app/"})
 public class TypeUserController {
 
     private final TypeUserDAO typeUserRepo;
